5ba5d55152abb0d2ecb55513a4e43ce7e4646485
Massaki Archambault <archambault.massaki@gmail.com>
2015-02-16 15:07:13 -0500

    Ajouté les contrôles de la vidéo

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
 	$(window).scroll(miniParallaxe).resize(miniParallaxe).load(miniParallaxe);
+	$(".play-button-container").click(startVideo)
 })
 
 function miniParallaxe(){
 	var percent = $(window).scrollTop() / Math.max(10000 ,$("body").height()) * 100;
 	$("body").css("background-position", "0 " + percent + "%");
+}
+
+function startVideo(){
+	$(this).hide().parent(".video-container").find("video").attr("controls", "").get(0).play();
 }
